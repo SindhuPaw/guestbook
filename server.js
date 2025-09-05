19fcba7392e9d1d101c920dd95e11a88c3900eae
SindhuPaw <sindhupawitra@gmail.com>
2025-09-05 16:34:36 +0700

    change to dynamic port

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json()); // Middleware untuk membaca data JSON
 app.use(express.urlencoded({ extended: true })); // Middleware untuk membaca data dari form
